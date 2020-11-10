55016da1fb1303c77374fe2836f5a63c141b84f4
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2020-11-10 13:08:52 +0300

    function name => task[number]

--- src/task/index.js
+++ src/task/index.js
@@ -1,9 +1,9 @@
-const {filterGoods} = require('./task1');
-const {findMostExpensiveGood} = require('./task2');
-const {remapGoods} = require('./task3');
+const {task1} = require('./task1');
+const {task2} = require('./task2');
+const {task3} = require('./task3');
 
 module.exports = {
-filterGoods,
-findMostExpensiveGood,
-remapGoods,
+task1,
+task2,
+task3,
 }
