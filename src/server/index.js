8aebeeb1ebea03d772a6f6c461da5cca6bfc427d
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2020-11-26 22:22:14 +0300

    added graceful shutdown

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const http = require('http');
 const requestHandler = require('./requestHandler');
 
@@ -11,6 +9,20 @@ function start() {
   );
 }
 
+function stop(callback) {
+  server.close((err) => {
+    if (err) {
+      console.error(err, 'Failed to close server!');
+      callback();
+      return;
+    }
+
+    console.log('Server has been stopped.');
+    callback();
+  });
+}
+
 module.exports = {
   start,
+  stop,
 };
