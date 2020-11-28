af80d8340146071f53eb89ca8be43edf7597693a
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2020-11-29 00:07:25 +0300

    added things for optimization

--- src/server/controller.js
+++ src/server/controller.js
@@ -1,7 +1,123 @@
 const fs = require('fs');
 const path = require('path');
+const { pipeline } = require('stream');
+const { createGunzip } = require('zlib');
+const { promisify } = require('util');
+
+const { nanoid } = require('nanoid');
+
+const { createCsvToJson } = require('../upload/csv-to-json');
+const { createJsonOptimizer } = require('../upload/optimize-json');
+const tasks = require('../task');
+
+const goodsList = require('../../goods.json');
+
+const promisifiedPipeline = promisify(pipeline);
 
 const pathToFile = path.resolve(__dirname, '../../', 'goods.json');
+
+const store = {
+  local: goodsList,
+  uploaded: {},
+  current: goodsList,
+};
+
+function filterGoods(property, value) {
+  return tasks.filterGoods(store.current, property, value);
+}
+
+function findMostExpensiveGoods() {
+  return tasks.FindMostExpensiveGoods;
+}
+
+function remapGoods() {
+  return tasks.remapGoods(store.current);
+}
+
+function setStore(newProducts) {
+  store.uploaded = newProducts;
+}
+
+function switchStore() {
+  store.current = store.current === goodsList ? store.uploaded : goodsList;
+}
+
+async function uploadCsv(inputStream) {
+  const uploadDir = process.env.UPLOAD_DIR;
+
+  const gunzip = createGunzip();
+
+  const timestamp = Date.now();
+  const filePath = `${uploadDir}${timestamp}-${nanoid()}.json`;
+  const outputStream = fs.createWriteStream(filePath);
+  const csvToJson = createCsvToJson();
+
+  try {
+    await promisifiedPipeline(inputStream, gunzip, csvToJson, outputStream);
+  } catch (err) {
+    console.error('CSV pipeline failed', err);
+
+    try {
+      await fs.promises.rm(filePath);
+    } catch (rmErr) {
+      console.error(`Unable to remove JSON ${filePath}`, rmErr);
+      throw new Error('Unable to remove JSON');
+    }
+  }
+}
+
+async function getUploadFileList() {
+  const uploadDir = process.env.UPLOAD_DIR;
+
+  try {
+    const files = await fs.promises.readdir(uploadDir);
+    const jsonFiles = files.filter((filename) => filename.toLowerCase().endsWith('.json'));
+    return { files: jsonFiles };
+  } catch (err) {
+    console.error(`Unable to scan directory ${uploadDir}`, err);
+    throw new Error('Unable to scan directory');
+  }
+}
+
+async function optimizeJson(filename) {
+  const uploadDir = process.env.UPLOAD_DIR;
+  const filePath = path.join(uploadDir, filename);
+
+  const optimizedDir = process.env.OPTIMIZED_DIR;
+  const optimizedFilePath = path.join(optimizedDir, filename);
+
+  const fileReader = fs.createReadStream(filePath);
+
+  const optimizedProducts = [];
+  const optimizer = createJsonOptimizer(optimizedProducts);
+
+  try {
+    await promisifiedPipeline(fileReader, optimizer);
+  } catch (err) {
+    console.error('Optimization pipeline failed', err);
+  }
+
+  try {
+    const optimizedJson = JSON.stringify(optimizedProducts);
+    await fs.promises.writeFile(optimizedFilePath, optimizedJson);
+  } catch (err) {
+    console.error(`Unable to write optimized JSON to ${optimizedDir}`, err);
+    throw new Error('Unable to write optimized JSON');
+  }
+
+  try {
+    await fs.promises.rm(filePath);
+  } catch (err) {
+    console.error(`Unable to remove JSON ${filePath}`, err);
+    throw new Error('Unable to remove JSON');
+  }
+
+  const totalQuantity = optimizedProducts.reduce((total, product) => {
+    return total + product.quantity;
+  }, 0);
+
+  console.log(`Optimization process finished. Total product quantity: ${totalQuantity}`);
+}
 
 const goods = require('../../goods.json');
 const { task1: firstTask, task2: secondTask, task3: thirdTask } = require('../task');
@@ -60,9 +176,17 @@ async function addDiscount(response) {
 }
 
 module.exports = {
+  filterGoods,
+  findMostExpensiveGoods,
+  remapGoods,
   task1,
   task2,
   task3,
   newFile,
   addDiscount,
+  setStore,
+  switchStore,
+  uploadCsv,
+  getUploadFileList,
+  optimizeJson,
 };
