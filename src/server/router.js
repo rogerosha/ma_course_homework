9f79f3377eb8fed8ea95370fbf4bd64d1d27d96b
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2020-11-28 18:26:53 +0300

    added new async functions

--- src/server/router.js
+++ src/server/router.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const {
-  task1: filterGoods,
-  task2: findMostExpensiveGoods,
-  task3: remapGoods,
-  newFile,
-  addDiscount,
+  task1,
+  task2,
+  task3,
+  setStore,
+  switchStore,
+  uploadCsv,
+  getUploadFileList,
+  optimizeJson,
 } = require('./controller.js');
 
 function notFound(res) {
@@ -11,20 +16,105 @@ function notFound(res) {
   res.end('404 page not found');
 }
 
-module.exports = (request, response) => {
-  const { url, method, queryParams, body: data } = request;
+function makeEndResponse(response) {
+  response.statusCode = 200;
+  console.warn('Cannot end response. Response is finished.');
+}
+
+async function handleStreamRoutes(request, response) {
+  const endResponse = makeEndResponse(response);
+  const { url, method } = request;
 
   response.setHeader('Content-Type', 'application/json');
-  if (method === 'GET' && url.startsWith('/task1?')) {
-    return filterGoods(response, queryParams);
+
+  if (method === 'PUT' && url === '/upload/csv') {
+    try {
+      await uploadCsv(request);
+    } catch (err) {
+      console.error('Failed to upload CSV', err);
+
+      response.statusCode = 500;
+      response.end('500 server error');
+      return;
+    }
+    endResponse({ status: 'ok' });
+    return;
+  }
+  notFound(response);
+}
+
+async function handleRoutes(request, response) {
+  const endResponse = makeEndResponse(response);
+  const { body, url, method } = request;
+  const urlPath = path.parse(url.pathname);
+
+  if (method === 'GET' && url.pathname === '/task1?') {
+    const property = url.searchParams.get('property');
+    const value = url.searchParams.get('value');
+    const result = task1(property, value);
+
+    endResponse(result);
+    return;
   }
 
-  if (method === 'GET' && url === '/task2') return findMostExpensiveGoods(response);
+  if (method === 'GET' && url.pathname === '/task2') {
+    const result = task2();
+    endResponse(result);
+    return;
+  }
 
-  if (method === 'GET' && url === '/task3') return remapGoods(response);
+  if (method === 'GET' && url.pathname === '/task3') {
+    const result = task3();
+    endResponse(result);
+    return;
+  }
 
-  if (method === 'GET' && url === '/products/discounts') return addDiscount(response);
+  if (method === 'POST' && url === '/store') {
+    setStore(body);
+    endResponse();
+    return;
+  }
 
-  if (method === 'POST' && url === '/newFile') return newFile(data, response);
-  return notFound(response);
+  if (method === 'GET' && url.pathname === '/store/switch') {
+    switchStore();
+    endResponse();
+    return;
+  }
+
+  if (method === 'GET' && url.pathname === '/upload') {
+    try {
+      const fileList = await getUploadFileList();
+      endResponse(fileList);
+    } catch (err) {
+      console.error('Failed to get file list', err);
+      endResponse({ status: 'error' }, 500);
+    }
+    return;
+  }
+
+  if (method === 'POST' && urlPath.dir === '/upload/optimize') {
+    const fileName = urlPath.base;
+
+    const { files: uploadFiles } = await getUploadFileList();
+    if (!uploadFiles.includes(fileName)) {
+      console.error(`File not found: ${fileName}`);
+      endResponse({ status: 'error', message: 'File not found' }, 404);
+      return;
+    }
+
+    optimizeJson(fileName).catch((err) => {
+      console.error('Failed start optimization process', err);
+      endResponse({ status: 'error' }, 500);
+    });
+
+    endResponse({ status: 'processing' }, 202);
+    return;
+  }
+
+  notFound(response);
+}
+
+module.exports = {
+  handleRoutes,
+  handleStreamRoutes,
 };
