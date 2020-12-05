703895b251761a0a6148ac1e2ea4fc1db5512627
Kate Sharova <rogerosha@gmail.com>
2020-12-05 23:10:36 +0300

    fixed csv-to-json file errors

--- src/utils/csv-to-json.js
+++ src/utils/csv-to-json.js
@@ -1,25 +1,25 @@
 const { Transform } = require('stream');
 
-function splitGoods(csvKeys, newRow) {
+function makeGoods(csvKeys, newRow) {
   return newRow.map((row) => {
-    const goodsValue = row.split('');
+    const csvValues = row.split(',');
 
-const goodIncoming = csvKeys.map((key, i) => {
-  let value = csvValues[i] ? 'N/A':
+    const goodEntries = csvKeys.map((key, i) => {
+      let value = csvValues[i] || 'N/A';
 
-  if (key === 'quantity') {
-    value = Number.parseInt(csvValues[i], 10);
-    if (Number.isNaN(value)) {
-      console.error('Uncorrect quantity', value);
-      value = 0;
-    }
-  }
-  if (key === 'price') value = '$${value}';
-  
-  return [key, value];
-});
-const good = Object.fromEntries(goofEntries);
-return JSON.stringify(good);
+      if (key === 'quantity') {
+        value = Number.parseInt(csvValues[i], 10);
+        if (Number.isNaN(value)) {
+          console.error('Uncorrect quantity', value);
+          value = 0;
+        }
+      }
+      if (key === 'price') value = `$${value}`;
+
+      return [key, value];
+    });
+    const good = Object.fromEntries(goodEntries);
+    return JSON.stringify(good);
   });
 }
 
@@ -28,7 +28,7 @@ function createCsvToJson() {
   let goodFragment;
 
   const transform = (chunk, encoding, callback) => {
-    const stringGoods = chunk.toString().split('\n');
+    const newRow = chunk.toString().split('\n');
     let output = '';
 
     if (!csvKeys) {
@@ -39,7 +39,7 @@ function createCsvToJson() {
     }
 
     if (goodFragment) {
-      newRow[0] = '${goodFragment}$(newRow[0])';
+      newRow[0] = `${goodFragment}${newRow[0]}`;
     }
 
     goodFragment = newRow.pop();
@@ -50,19 +50,19 @@ function createCsvToJson() {
     }
 
     const stringifiedGoods = makeGoods(csvKeys, newRow);
-    output += stringifiedProducts.join(',\n');
+    output += stringifiedGoods.join(',\n');
 
     callback(null, output);
-    }
   };
 
   const flush = (callback) => {
     console.log('No more data to read.');
     callback(null, '\nFinish!');
   };
-return new Transform({ transform, flush });
-
+  return new Transform({ transform, flush });
+}
 
 module.exports = {
+  makeGoods,
   createCsvToJson,
 };
