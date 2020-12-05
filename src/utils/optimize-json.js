6561434ee7c066f1313fd1245db1144da85adb79
Kate Sharova <rogerosha@gmail.com>
2020-12-05 12:38:59 +0300

    added an optimize logic

---
+++ src/utils/optimize-json.js
@@ -0,0 +1,59 @@
+const { Writable } = require('stream');
+
+function applyToOptimized(optimizedGoods, stringifiedGoods) {
+  const unoptimizedGoods = stringifiedGoods.map((good) => {
+    try {
+      return JSON.parse(good);
+    } catch (err) {
+      console.error('Failed to parse JSON good', err);
+      return {};
+    }
+  });
+
+  unoptimizedGoods.forEach((good) => {
+    const optimizedGood = optimizedGoods.find((op) => {
+      return op.type === good.type && op.color === good.color && op.price === good.price;
+    });
+
+    if (optimizedGood) {
+      optimizedGood.quantity += good.quantity;
+      return;
+    }
+
+    optimizedGood.push(good);
+  });
+}
+
+function createJsonOptimizer(optimizedGood) {
+  let isFirstChunk = true;
+  let productFragment = '';
+
+  const write = (chunk, encoding, callback) => {
+    let stringifiedChunk = chunk.toString();
+
+    if (isFirstChunk) {
+      stringifiedChunk = stringifiedChunk.slice(1); // remove leading '['
+      isFirstChunk = false;
+    } else {
+      stringifiedChunk = productFragment + stringifiedChunk;
+    }
+
+    const stringifiedGoods = stringifiedChunk.split(',\n');
+
+    productFragment = stringifiedGoods.pop(); // move incomplete part
+
+    if (stringifiedGoods.length === 0) {
+      callback();
+      return;
+    }
+
+    applyToOptimized(optimizedGood, stringifiedGoods);
+    callback();
+  };
+
+  return new Writable({ write });
+}
+
+module.exports = {
+  createJsonOptimizer,
+};
