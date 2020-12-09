1a87262604ed126abc1fadf87987c5e3759bf5d9
Kate Sharova <rogerosha@gmail.com>
2020-12-09 19:55:58 +0300

    wrote logic for basic authorization

--- src/server/middlewares/authorizeCheck.js
+++ src/server/middlewares/authorizeCheck.js
@@ -1,14 +1,16 @@
 const { name, password } = require('../../config/index.js');
 
-function checkLogIn(req, res, next) {
-  const auth = req.headers.authorization.split(' ');
-  const { userName, userPass } = Buffer.from(auth, 'base64').toString();
-  if (name === userName && password === userPass) {
-    return next();
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = password.verify(token, 'base64');
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'Invalid user ID';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({ error: 'Invalid request!' });
   }
-  return 0;
-}
-
-module.exports = {
-  checkLogIn,
 };
