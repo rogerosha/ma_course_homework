8447606b0dbe70b43d7a6fe887271ee06fc6dac1
Kate Sharova <rogerosha@gmail.com>
2020-12-13 21:55:03 +0300

    cut end response

--- src/server/router.js
+++ src/server/router.js
@@ -16,12 +16,15 @@ function notFound(res) {
   res.end('404 page not found');
 }
 
-function makeEndResponse(response, message, statusCode = 200) {
-  response.statusCode = statusCode;
-  response.end(JSON.stringify(message));
+function makeEndResponse(response) {
+  return (message, statusCode = 200) => {
+    response.statusCode = statusCode;
+    response.end(JSON.stringify(message));
+  };
 }
 
 async function handleStreamRoutes(request, response) {
+  const endResponse = makeEndResponse(response);
   const { url, method } = request;
   response.setHeader('Content-Type', 'application/json');
 
@@ -30,10 +33,10 @@ async function handleStreamRoutes(request, response) {
       await uploadCsv(request);
     } catch (err) {
       console.error('Failed to upload CSV', err);
-      makeEndResponse(response, { status: err.message }, 500);
+      endResponse({ status: err.message }, 500);
       return;
     }
-    makeEndResponse(response, { status: 'everything is okay' });
+    endResponse({ status: 'everything is okay' });
     return;
   }
   notFound(response);
@@ -42,47 +45,48 @@ async function handleStreamRoutes(request, response) {
 async function handleRoutes(request, response) {
   const { body, url, method, queryParams } = request;
   const urlPath = path.parse(url);
+  const endResponse = makeEndResponse(response);
   response.setHeader('Content-Type', 'application/json');
 
   if (method === 'GET' && url.split('?')[0] === '/task1') {
     const { property, value } = queryParams;
     const result = task1(property, +value);
 
-    makeEndResponse(response, result);
+    endResponse(result);
     return;
   }
 
   if (method === 'GET' && url === '/task2') {
     const result = task2();
-    makeEndResponse(response, result);
+    endResponse(result);
     return;
   }
 
   if (method === 'GET' && url === '/task3') {
     const result = task3();
-    makeEndResponse(response, result);
+    endResponse(result);
     return;
   }
 
   if (method === 'POST' && url === '/store') {
     setStore(body);
-    makeEndResponse(response, { status: 'ok' });
+    endResponse({ status: 'ok' });
     return;
   }
 
   if (method === 'GET' && url === '/store/switch') {
     switchStore();
-    makeEndResponse(response, { status: 'ok' });
+    endResponse({ status: 'ok' });
     return;
   }
 
   if (method === 'GET' && url === '/upload') {
     try {
       const fileList = await getUploadFileList();
-      makeEndResponse(response, fileList);
+      endResponse(fileList);
     } catch (err) {
       console.error('Failed to get file list', err);
-      makeEndResponse(response, { status: err.message }, 500);
+      endResponse({ status: err.message }, 500);
     }
     return;
   }
@@ -92,14 +96,14 @@ async function handleRoutes(request, response) {
     try {
       await getUploadFileList(request);
     } catch (err) {
-      makeEndResponse(response, { status: err.message }, 500);
+      endResponse({ status: err.message }, 500);
       return;
     }
     optimizeJson(fileName).catch((err) => {
       console.error('Something goes wrong', err);
-      makeEndResponse(response, { status: err.message }, 500);
+      endResponse({ status: err.message }, 500);
     });
-    makeEndResponse(response, { status: 'okay' }, 202);
+    endResponse({ status: 'okay' }, 202);
     return;
   }
 
