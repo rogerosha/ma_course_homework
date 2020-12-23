66b5eb537d3ed17c945663af701f691fd14f460b
Kate Sharova <rogerosha@gmail.com>
2020-12-23 22:28:40 +0300

    rewrote the csvUploadFile logic

--- src/server/csvUploadFile.js
+++ src/server/csvUploadFile.js
@@ -3,20 +3,41 @@ const { pipeline } = require('stream');
 const { promisify } = require('util');
 const fs = require('fs');
 const { nanoid } = require('nanoid');
+const es = require('event-stream');
 
 const { config } = require('../config');
+const { createCsvToJson } = require('../utils/csv-to-json');
 
 const promisifiedPipeline = promisify(pipeline);
 
-const { createCsvToJson } = require('../utils/csv-to-json');
-
 async function csvUploadFile(inputStream) {
+  const { uploadDir } = config;
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  const timestamp = Date.now();
+  const fileName = `${timestamp}-${nanoid()}.json`;
+  const filePath = `${uploadDir}/${fileName}`;
+
   const gunzip = createGunzip();
-  const filePath = `${config.uploadDir}/${Date.now()}-${nanoid(8)}.json`;
-  const outputStream = fs.createWriteStream(`${filePath}`);
   const csvToJson = createCsvToJson();
-  console.log(csvToJson);
-  promisifiedPipeline(inputStream, gunzip, csvToJson, outputStream);
+  const outputStream = fs.createWriteStream(filePath);
+
+  try {
+    await promisifiedPipeline(inputStream, gunzip, es.split(), csvToJson, outputStream);
+  } catch (err) {
+    console.error('CSV pipeline failed', err);
+
+    try {
+      await fs.unlinkSync(filePath);
+    } catch (rmErr) {
+      console.error(`Unable to remove JSON ${filePath}`, rmErr);
+      throw new Error('Unable to remove JSON');
+    }
+  }
+
+  return fileName;
 }
 
 module.exports = {
