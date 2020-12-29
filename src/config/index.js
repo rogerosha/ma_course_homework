40b40e094aef869dd21f6771d4cab01bec25c9b5
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2020-12-29 23:02:28 +0300

    added db to config

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,14 @@ const config = {
   port: Number(process.env.PORT) || 3000,
   uploadDir: process.env.UPLOAD_DIR || closeProgram('No upload dir'),
   optimizedDir: process.env.OPTIMIZED_DIR || closeProgram('No optimized dir'),
+
+  db: {
+    user: process.env.DB_USER || closeProgram('CLOSED: DB_USER is not defined'),
+    host: process.env.DB_HOST || closeProgram('CLOSED: DB_HOST is not defined'),
+    port: process.env.DB_PORT || closeProgram('CLOSED: DB_PORT is not defined'),
+    database: process.env.DB_NAME || closeProgram('CLOSED: DB_NAME is not defined'),
+    password: process.env.DB_PASS || closeProgram('CLOSED: DB_PASS is not defined'),
+  },
 };
 
 const user = {
