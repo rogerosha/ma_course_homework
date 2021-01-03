6ded94c5d87004c1a0e7420859867f02e46815fe
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-01-03 22:06:50 +0300

    wrote index.js for knex

--- src/db/index.js
+++ src/db/index.js
@@ -1,157 +1,56 @@
-const { Pool } = require('pg');
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable global-require */
+/* eslint-disable no-shadow */
 
-const { config } = require('../config');
+const {
+  db: { config, defaultType },
+  closeProgram,
+} = require('../config');
 
-const client = new Pool(config.db);
+const db = {};
+const type = defaultType;
 
-async function createTable() {
+async function init() {
   try {
-    await client.query(`CREATE SEQUENCE IF NOT EXISTS products_id_seq START 1; CREATE TABLE IF NOT EXISTS "public"."products" (
-      "type" character varying(255) NOT NULL,
-      "color" character varying(255) NOT NULL,
-      "quantity" integer NOT NULL,
-      "price" numeric NOT NULL,
-      "created_at" timestamptz,
-      "updated_at" timestamptz,
-      "deleted_at" timestamptz,
-      "id" integer DEFAULT nextval('products_id_seq') NOT NULL
-  ) WITH (oids = false)`);
-    console.log('Table has been created');
-  } catch (err) {
-    console.error(err.message || err);
-    throw err;
-  }
-}
-
-async function testConnection() {
-  try {
-    console.log('Hello from pg testConnection');
-    await client.query('SELECT NOW()');
-  } catch (err) {
-    console.error(err.message || err);
-    throw err;
-  }
-}
-
-async function close() {
-  console.log('INFO: Closing pg DB wrapper');
-  client.end();
-}
-
-async function createNewProduct({ type, color, price = 0, quantity = 1 }) {
-  try {
-    if (!type) {
-      throw new Error('ERROR: No product type defined');
+    for (const [key, value] of Object.entries(config)) {
+      const wrapper = require(`./${key}`)(value);
+      await wrapper.testConnection();
+      console.log(`INFO: DB wrapper for ${key} initiated`);
+      db[key] = wrapper;
     }
-    if (!color) {
-      throw new Error('ERROR: No product color defined');
-    }
-    const timestamp = new Date();
-
-    const res = await client.query(
-      'INSERT INTO products(type, color, price, quantity, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-      [type, color, price, quantity, timestamp, timestamp, null],
-    );
-
-    console.log(`DEBUG: New product created: ${JSON.stringify(res.rows[0])}`);
-    return res.rows[0];
   } catch (err) {
-    console.error(err.message || err);
-    throw err;
+    closeProgram(`FATAL: ${err.message || err}`);
   }
 }
 
-async function getProduct(id) {
-  try {
-    if (!id) {
-      throw new Error('ERROR: No product id defined');
-    }
-    const res = await client.query('SELECT * FROM products WHERE id = $1 AND deleted_at IS NULL', [
-      id,
-    ]);
-
-    return res.rows[0];
-  } catch (err) {
-    console.error(err.message || err);
-    throw err;
+async function end() {
+  for (const [key, value] of Object.entries(db)) {
+    await value.close();
+    console.log(`INFO: DB wrapper for ${key} was closed`);
   }
 }
 
-async function updateProduct({ id, ...product }) {
-  try {
-    if (!id) {
-      throw new Error('ERROR: No product id defined');
-    }
-    const query = [];
-    const values = [];
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const [index, [key, value]] of Object.entries(product).entries()) {
-      query.push(`${key} = $${index + 1}`);
-      values.push(value);
-    }
-
-    if (!values.length) {
-      throw new Error('ERROR: Nothing to update');
-    }
-
-    values.push(id);
-
-    const res = await client.query(
-      `UPDATE products SET ${query.join(',')} WHERE id = $${values.length} RETURNING *`,
-      values,
-    );
-    console.log(`DEBUG: Product updated: ${JSON.stringify(res.rows[0])}`);
-    return res.rows[0];
-  } catch (err) {
-    console.error(err.message || err);
-    throw err;
+async function setType(t) {
+  if (!t || !db[t]) {
+    console.log(`WARNING: Cannot find provided DB type!`);
+    return false;
   }
+  // eslint-disable-next-line no-const-assign
+  type = t;
+  console.log(`INFO: The DB type has been changed to ${t}`);
+  return true;
 }
 
-async function createProduct({ type, color, price = 0, quantity = 1 }) {
-  const res = await client.query(
-    'SELECT * FROM products WHERE type = $1 AND color = $2 AND price = $3 AND deleted_at IS NULL',
-    [type, color, price],
-  );
-  const product = res.rows[0];
-  if (product) {
-    return updateProduct({ ...product, quantity: product.quantity + 1 });
-  }
-  return createNewProduct({ type, color, price, quantity });
-}
+const getType = () => type;
 
-async function deleteProduct(id) {
-  try {
-    if (!id) {
-      throw new Error('ERROR: No product id defined');
-    }
-    // await client.query('DELETE FROM products WHERE id = $1', [id]);
-    await client.query('UPDATE products SET deleted_at = $1 WHERE id = $2', [new Date(), id]);
-    return true;
-  } catch (err) {
-    console.error(err.message || err);
-    throw err;
-  }
-}
-
-async function getAllProducts() {
-  try {
-    const res = await client.query('SELECT * FROM products WHERE deleted_at IS NULL');
-    return res.rows;
-  } catch (err) {
-    console.error(err.message || err);
-    throw err;
-  }
-}
+const dbWrapper = (t) => db[t] || db[type];
 
 module.exports = {
-  createTable,
-  testConnection,
-  close,
-  createProduct,
-  getProduct,
-  updateProduct,
-  deleteProduct,
-  getAllProducts,
+  init,
+  end,
+  setType,
+  getType,
+  dbWrapper,
 };
