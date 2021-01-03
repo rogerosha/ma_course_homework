f1da1e9fb33e20ac8acae3e24f53f5decf31293d
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-01-03 22:06:18 +0300

    updated config according to knex

--- src/config/index.js
+++ src/config/index.js
@@ -7,11 +7,31 @@ const config = {
   optimizedDir: process.env.OPTIMIZED_DIR || closeProgram('No optimized dir'),
 
   db: {
-    user: process.env.DB_USER || closeProgram('CLOSED: DB_USER is not defined'),
-    host: process.env.DB_HOST || closeProgram('CLOSED: DB_HOST is not defined'),
-    port: process.env.DB_PORT || closeProgram('CLOSED: DB_PORT is not defined'),
-    database: process.env.DB_NAME || closeProgram('CLOSED: DB_NAME is not defined'),
-    password: process.env.DB_PASS || closeProgram('CLOSED: DB_PASS is not defined'),
+    defaultType: process.env.DB_WRAPPER_TYPE || 'knex',
+    config: {
+      knex: {
+        client: 'postgresql',
+        connection: {
+          user: process.env.DB_USER || closeProgram('CLOSED: DB_USER is not defined'),
+          host: process.env.DB_HOST || closeProgram('CLOSED: DB_HOST is not defined'),
+          port: process.env.DB_PORT || closeProgram('CLOSED: DB_PORT is not defined'),
+          database: process.env.DB_NAME || closeProgram('CLOSED: DB_NAME is not defined'),
+          password: process.env.DB_PASS || closeProgram('CLOSED: DB_PASS is not defined'),
+        },
+        pool: {
+          min: 2,
+          max: 10,
+        },
+        debug: true,
+      },
+      pg: {
+        user: process.env.DB_USER || closeProgram('CLOSED: DB_USER is not defined'),
+        host: process.env.DB_HOST || closeProgram('CLOSED: DB_HOST is not defined'),
+        port: process.env.DB_PORT || closeProgram('CLOSED: DB_PORT is not defined'),
+        database: process.env.DB_NAME || closeProgram('CLOSED: DB_NAME is not defined'),
+        password: process.env.DB_PASS || closeProgram('CLOSED: DB_PASS is not defined'),
+      },
+    },
   },
 };
 
