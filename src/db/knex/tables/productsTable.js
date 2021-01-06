c43eddc63e3927d78706c0b4f49edf608673b568
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-01-06 19:29:51 +0300

    used ColorsTable instead of color column

--- src/db/knex/tables/productsTable.js
+++ src/db/knex/tables/productsTable.js
@@ -1,14 +1,19 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
+const { request } = require('express');
 const Knex = require('knex');
+const { ColorsTable } = require('./colorsTable');
 
 class ProductsTable {
   /**
    * @param {Knex} knex - Knex client
+   * @param {ColorsTable} colorsTable
    */
-  constructor(knex) {
+  constructor(knex, colorsTable) {
     this.knex = knex;
+    this.colorsTable = colorsTable;
 
     this.TABLE_NAME = 'products';
+
     this.DEFAULT_PRODUCT = {
       price: 0,
       quantity: 1,
@@ -18,85 +23,84 @@ class ProductsTable {
   async createTable() {
     const isTableExists = await this.knex.schema.hasTable(this.TABLE_NAME);
     if (isTableExists) {
-      console.log('The table is already created');
+      console.log(`Table ${this.TABLE_NAME} is already created`);
     } else {
       await this.knex.schema.createTable(this.TABLE_NAME, (table) => {
         table.increments();
-        table.string('type').notNullable();
-        table.string('color').notNullable();
-        table.integer('quantity').notNullable().defaultTo(this.DEFAULT_PRODUCT.quantity);
+        table.integer('color_id').references('id').inTable('colors').notNullable();
         table.decimal('price').notNullable().defaultTo(this.DEFAULT_PRODUCT.price);
+        table.unique(['color_id']);
+
+        table.string('type').notNullable();
+        table.integer('quantity').notNullable().defaultTo(this.DEFAULT_PRODUCT.quantity);
         table.timestamps(false, true);
         table.timestamp('deleted_at');
       });
-      console.log('The table has been created');
+      console.log(`Table ${this.TABLE_NAME} has been created`);
     }
   }
 
   // Requests
 
   async createProduct(requestProduct) {
-    const productsResult = await this.knex(this.TABLE_NAME)
-      .where({
-        type: requestProduct.type,
-        color: requestProduct.color,
-        price: requestProduct.price || this.DEFAULT_PRODUCT.price,
-      })
-      .whereNull('deleted_at');
+    const colorId = await this.colorsTable.getColorIdByColor(requestProduct.color);
 
-    const foundProduct = productsResult[0];
+    if (!colorId) {
+      throw new Error('No such color_id in the colors table. Please, create the color first');
+    }
 
-    return foundProduct
-      ? this.updateProduct({ ...foundProduct, quantity: foundProduct.quantity + 1 })
-      : this.createNewProduct(requestProduct);
+    const productCopy = JSON.parse(JSON.stringify(requestProduct));
+    delete productCopy.id;
+    delete productCopy.color;
+    productCopy.quantity = productCopy.quantity || 1;
+    productCopy.color_id = colorId;
+
+    const [createdProduct] = await this.knex(this.TABLE_NAME)
+      .insert(productCopy)
+      .returning('*')
+      .onConflict(['color_id'])
+      .merge({ quantity: this.knex.raw(`products.quantity + ${productCopy.quantity}`) })
+      .returning('*');
+
+    return createdProduct;
   }
 
   async updateProduct({ id, ...product }) {
-    try {
-      if (!id) {
-        throw new Error('ERROR: No product id defined');
-      }
-
-      if (!Object.keys(product).length) {
-        throw new Error('ERROR: Nothing to update');
-      }
-
-      const updateResult = await this.knex(this.TABLE_NAME)
-        .update(product)
-        .where('id', id)
-        .returning('*');
-
-      const updatedProduct = updateResult[0];
-
-      console.log(`DEBUG: Product updated: ${JSON.stringify(updatedProduct)}`);
-      return updatedProduct;
-    } catch (err) {
-      console.error(err.message || err);
-      throw err;
+    if (!id) {
+      throw new Error('ERROR: No product id defined');
     }
-  }
 
-  async createNewProduct(product) {
-    try {
-      if (!product.type) {
-        throw new Error('ERROR: No product type defined');
-      }
-
-      if (!product.color) {
-        throw new Error('ERROR: No product color defined');
-      }
-
-      const productCopy = JSON.parse(JSON.stringify(product));
-      delete productCopy.id;
-
-      const createResult = await this.knex(this.TABLE_NAME).insert(productCopy).returning('*');
-      const createdProduct = createResult[0];
-      console.log(`DEBUG: New product created: ${JSON.stringify(createdProduct)}`);
-      return createdProduct;
-    } catch (err) {
-      console.error(err.message || err);
-      throw err;
+    if (!Object.keys(product).length) {
+      throw new Error('ERROR: Nothing to update');
     }
+
+    const updateProduct = {};
+    const updateRequests = Object.keys(product).map(async (key) => {
+      switch (key) {
+        case 'color':
+          updateProduct.color_id = (await this.colorsTable.createColor(product.color)).id;
+          break;
+
+        default:
+          updateProduct[key] = product[key];
+          break;
+      }
+
+      return 0;
+    });
+
+    await Promise.all(updateRequests);
+    updateProduct.updated_at = new Date();
+
+    console.log({ id, updateProduct });
+
+    const [updatedProduct] = await this.knex(this.TABLE_NAME)
+      .update(updateProduct)
+      .where({ id })
+      .returning('*');
+
+    console.log(`DEBUG: Product updated: ${JSON.stringify(updatedProduct)}`);
+    return updatedProduct;
   }
 
   async getProduct(id) {
@@ -105,8 +109,15 @@ class ProductsTable {
         throw new Error('ERROR: No product id defined');
       }
 
-      const foundResult = await this.knex(this.TABLE_NAME).where('id', id).whereNull('deleted_at');
-      const foundProduct = foundResult[0];
+      const [foundProduct] = await this.knex(this.TABLE_NAME)
+        .where(`${this.TABLE_NAME}.id`, id)
+        .whereNull('deleted_at')
+        .join(
+          this.colorsTable.TABLE_NAME,
+          `${this.TABLE_NAME}.color_id`,
+          `${this.colorsTable.TABLE_NAME}.id`,
+        );
+      // .select(`${this.colorsTable.TABLE_NAME}.value`);
 
       return foundProduct;
     } catch (err) {
@@ -121,9 +132,9 @@ class ProductsTable {
         throw new Error('ERROR: No product id defined');
       }
 
-      await this.knex(this.TABLE_NAME).where('id', id).update('deleted_at', new Date());
+      await this.knex(this.TABLE_NAME).where({ id }).update('deleted_at', new Date());
 
-      return true;
+      return { status: true };
     } catch (err) {
       console.error(err.message || err);
       throw err;
@@ -132,7 +143,14 @@ class ProductsTable {
 
   async getAllProducts() {
     try {
-      const foundProducts = await this.knex(this.TABLE_NAME).whereNull('deleted_at');
+      const foundProducts = await this.knex(this.TABLE_NAME)
+        .whereNull('deleted_at')
+        .join(
+          this.colorsTable.TABLE_NAME,
+          `${this.TABLE_NAME}.color_id`,
+          `${this.colorsTable.TABLE_NAME}.id`,
+        )
+        .select(`${this.colorsTable.TABLE_NAME}.value`);
       return foundProducts;
     } catch (err) {
       console.error(err.message || err);
